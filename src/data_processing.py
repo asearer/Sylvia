acb6f8de6f30d48a57b6b3408a9d6d124c70751d
asearer <amsearer84@gmail.com>
2024-05-13 17:18:35 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,14 @@
 # src/data_processing.py
 
 def preprocess_data(data):
-    
+    processed_data = []
+    for conversation in data:
+        # Placeholder preprocessing steps
+        processed_conversation = []
+        for message in conversation:
+            # Tokenization, lowercasing, etc.
+            processed_message = message.lower().split()
+            processed_conversation.append(processed_message)
+        processed_data.append(processed_conversation)
     return processed_data
+
